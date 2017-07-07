3a73f46d9d2956b8e29b4f5d203d4755d409ab92
Sloane Smith <sloanesmith@Sloanes-MacBook-Pro.local>
2017-07-07 15:39:37 -0700

    styled export csv modal

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { AppContainer } from "react-hot-loader";
-import Root from "./Root.jsx";
+import Root from "./Root";
 import * as PropTypes from "prop-types";
 
 
