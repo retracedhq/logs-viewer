e12e39fab5616b6b7dbade17099d24654911d5e7
Sloane Smith <s10dimensional@gmail.com>
2017-11-01 15:59:09 -0700

    Updated dark theme event color

--- src/components/modals/AccessTokensModal.jsx
+++ src/components/modals/AccessTokensModal.jsx
@@ -176,7 +176,7 @@ class AccessTokensModal extends React.Component {
                         }
                     </div>
                 <div className="flex flex-auto buttons justifyContent--flexEnd">
-                    <a className="u-padding--normal u-fontSize--normal u-color--curiousBlue" href="https://www.replicated.com/docs/kb/developer-resources/generate-api-token/" target="_blank">What is an API token?</a>
+                    <a className="u-padding--normal u-fontSize--normal u-color--curiousBlue" href="https://preview.staging.retraced.io/documentation/getting-started/embedded-viewer/#call-the-embed-function-to-initialize-the-viewer" target="_blank">What is an API token?</a>
                     <button className="Button primary u-marginLeft--normal" onClick={() => { this.setState({ creatingToken: true }) }}>Create Token</button>
                 </div>
                 </div>
@@ -203,7 +203,7 @@ class AccessTokensModal extends React.Component {
                     <div className="u-tokenIllustration u-padding--normal"></div>
                     <p className="u-fontWeight--medium u-paddingBottom--small u-width--full u-textAlign--center">You have not created any access tokens</p>
                     <button className="Button primary u-margin--small" onClick={() => { this.setState({ creatingToken: true }) }}>Create new token</button>
-                    <a className="u-padding--small u-textAlign--center u-display--block u-width--full  u-fontSize--normal u-color--curiousBlue" href="https://www.replicated.com/docs/kb/developer-resources/generate-api-token/" target="_blank">What is an API token?</a>
+                    <a className="u-padding--small u-textAlign--center u-display--block u-width--full  u-fontSize--normal u-color--curiousBlue" href="https://preview.staging.retraced.io/documentation/getting-started/embedded-viewer/#call-the-embed-function-to-initialize-the-viewer" target="_blank">What is an API token?</a>
                 </div>
         }
       </div>
