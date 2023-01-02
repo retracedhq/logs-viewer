10500ddbc878ab80d993457b80f13259723db00a
Deepak Prabhakara <deepak@boxyhq.com>
2023-01-02 20:54:35 +0000

    local dev made easy

---
+++ src/dev/LogsViewerWrapper.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import RetracedEventsBrowser from "../index";
+
+const apiKey = "dev";
+const projectId = "dev";
+const endpoint = "http://localhost:3000/auditlog";
+const groupId = "dev";
+const actorId = "dev";
+
+const LogsViewerWrapper = () => {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const urlWithQuery = `${endpoint}/publisher/v1/project/${projectId}/viewertoken?group_id=${groupId}&actor_id=${actorId}&is_admin=true`;
+    fetch(urlWithQuery, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Token token=${apiKey}`,
+      },
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setToken(data.token);
+        });
+
+        return;
+      }
+
+      throw new Error("Failed to fetch viewer token");
+    });
+  }, []);
+
+  if (!token) {
+    return <progress> </progress>;
+  }
+
+  return <RetracedEventsBrowser auditLogToken={token} />;
+};
+
+export default LogsViewerWrapper;
--- src/dev/index.jsx
+++ src/dev/index.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import RetracedEventsBrowser from "../index";
+import LogsViewerWrapper from "./LogsViewerWrapper";
 
-ReactDOM.render(
-  <RetracedEventsBrowser auditLogToken="123" />,
-  document.getElementById("app")
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LogsViewerWrapper />
+  </React.StrictMode>
 );
