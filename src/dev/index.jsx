fc7d91bf6dd6ec5db593bb77953db7996f0fa415
Deepak Prabhakara <deepak@boxyhq.com>
2023-01-16 18:15:05 +0000

    removed strictmode

--- src/dev/index.jsx
+++ src/dev/index.jsx
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom/client";
 import LogsViewerWrapper from "./LogsViewerWrapper";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <LogsViewerWrapper />
-  </React.StrictMode>
+  <LogsViewerWrapper />
 );
