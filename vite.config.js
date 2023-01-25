1a6acc9e8fb085300cb1b3145a7041d740419a8d
Deepak Prabhakara <deepak@boxyhq.com>
2023-01-25 09:43:26 +0000

    don't minify

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
+  minify: false,
   plugins: [
     react({
       babel: {
