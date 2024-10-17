6e8a8d068de18700f4b8c877ab6c6b487bb3bd8f
Aswin V <vaswin91@gmail.com>
2024-10-17 21:24:44 +0700

    Debug logging

--- src/components/containers/EventsBrowser.tsx
+++ src/components/containers/EventsBrowser.tsx
@@ -198,6 +198,7 @@ class EventsBrowser extends React.Component<EventBrowserProps, EventBrowserState
   }
 
   submitQuery(query, cursor) {
+    // console.log(`submitQuery called with query:${query}, cursor: ${cursor}`);
     const queryObj = {
       search_text: query,
       cursor,
